83b08054682dd62a4dc24794e747d359c39033df
jongsujin <jjs9836@naver.com>
2025-01-26 00:45:38 +0900

    Feat: DB 셋팅 및 공통 타입 설정

--- apps/types/Post.type.d.ts
+++ apps/types/Post.type.d.ts
@@ -1,11 +1,39 @@
-export interface Post {
-    id: number
-    title: string
-    date: string
-    description: string
-    gradient: string
-    image: string
-    content: string
-    tags: string[]
-  }
+export interface PostInitial {
+  title: string;
+  content: string;
+  thumbnail?: string;
+  tags: string[];
+}
+export interface Post extends PostInitial {
+  id: number;
+  slug: string;
+  publishedAt: Date;
+  createdAt: Date;
+  updatedAt: Date;
+  viewCount: number;
+}
 
+// 게시글 생성 시 사용할 DTO
+export interface CreatePostDTO extends PostInitial {}
+
+// 게시글 업데이트 시 사용할 DTO
+export interface UpdatePostDTO extends Partial<PostInitial> {}
+
+// 게시글 조회 시 사용할 DTO
+export interface PostListDTO {
+  id: number;
+  title: string;
+  slug: string;
+  thumbnail?: string;
+  tags: string[];
+  publishedAt: Date;
+  viewCount: number;
+}
+
+export interface ResponseProps<T> {
+  page: number;
+  hasNextPage: boolean;
+  content: T[];
+  totalCount: number;
+  hasPrevPage: boolean;
+}
