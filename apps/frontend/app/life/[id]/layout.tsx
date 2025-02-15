e49ab826fa8951a796a2329ee840b5af36b99ad6
jongsujin <jjs9836@naver.com>
2025-02-16 02:06:05 +0900

    fix: tech, life 삭제

--- apps/frontend/app/life/[id]/layout.tsx
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-    title: "Life",
-    description: "Life",
-}
-
-export default function LifeLayout({ children }: { children: React.ReactNode }) {
-    return <div>{children}</div>;
-}
--- apps/frontend/app/tech/[id]/layout.tsx
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-    title: "테크 기술 블로그 게시글",
-    description: "테크 기술 블로그 게시글",
-}
-
-export default function TechLayout({ children }: { children: React.ReactNode }) {
-    return <div>{children}</div>;
-}
