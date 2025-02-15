65af7e2afe6bfbcfda4c06b6e6930a473cc719ae
jongsujin <jjs9836@naver.com>
2025-02-16 01:48:33 +0900

    feat: 배포 1차 수정

--- apps/backend/src/config/constants.ts
+++ apps/backend/src/config/constants.ts
@@ -1,1 +1,5 @@
-export const WHITE_LIST = ["http://localhost:3000", "http://localhost:4000"];
+export const WHITE_LIST = [
+  "http://localhost:3000",
+  "http://localhost:4000",
+  "https://my-blog-app-git-main-jjs9836gmailcoms-projects.vercel.app/",
+];
--- apps/frontend/api/post/api.server.ts
+++ apps/frontend/api/post/api.server.ts
@@ -1,4 +1,4 @@
-'use server'
+// 'use server'
 import { API, APIError } from '../instance'
 import { Post } from '@my-blog/types'
 
