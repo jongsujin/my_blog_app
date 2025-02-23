e12c3fe9c4e1b5cd9ac21617438f2f24d8a834e5
jongsujin <jjs9836@naver.com>
2025-02-23 23:17:17 +0900

    fix: 블로그 메인 페이지 동적 렌더링으로 변경

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,6 +1,8 @@
 import { getPosts } from '@/api/post/api.server'
 import BlogList from './blog/_components/BlogList'
 
+export const dynamic = 'force-dynamic'
+
 export default async function BlogPage() {
   // 초기 데이터 가져옴
   const initialData = await getPosts(1, 3)
